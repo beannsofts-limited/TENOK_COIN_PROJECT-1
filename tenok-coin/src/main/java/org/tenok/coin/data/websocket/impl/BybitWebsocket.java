1e1d84cecf2fed16f6edc12c4ce9a7213536d6f5
lani009 <lani009@naver.com>
2021-01-25 13:26:26 +0900

    revision websocket

--- tenok-coin/src/main/java/org/tenok/coin/data/websocket/impl/BybitWebsocket.java
+++ tenok-coin/src/main/java/org/tenok/coin/data/websocket/impl/BybitWebsocket.java
@@ -1,19 +1,38 @@
 package org.tenok.coin.data.websocket.impl;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import javax.websocket.ClientEndpoint;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 
-public class BybitWebsocket {
+import org.json.simple.JSONObject;
+import org.tenok.coin.type.CoinEnum;
+import org.tenok.coin.type.IntervalEnum;
+
+@ClientEndpoint(encoders = { BybitEncoder.class }, decoders = { BybitDecoder.class })
+class BybitWebsocket implements Closeable {
+    Map<CoinEnum, Map<IntervalEnum, Consumer<JSONObject>>> kLineCallbackMap = null;
+
+    public BybitWebsocket() {
+        this.kLineCallbackMap = new HashMap<>();
+    }
+
     @OnOpen
     public void onOpen(Session session) {
-        
+
     }
 
     @OnMessage
-    public void onMessage() {
+    public void onMessage(JSONObject response) {
 
     }
 
@@ -24,6 +43,31 @@ public class BybitWebsocket {
 
     @OnError
     public void onError() {
+
+    }
+
+    /**
+     * websocket kline 실시간 처리
+     * 
+     * @param coinType
+     * @param interval
+     * @param consumer
+     */
+    public void registerKLineCallback(CoinEnum coinType, IntervalEnum interval, Consumer<JSONObject> consumer) {
+        kLineCallbackMap.putIfAbsent(coinType, new HashMap<>());
+        var previousValue = kLineCallbackMap.get(coinType).putIfAbsent(interval, consumer);
+        if (previousValue != null) {
+            throw new RuntimeException("RegisterKLineCallback request called twice by the equal parameter");
+        }
+    }
+
+    public void unregisterKLine(CoinEnum coinType, IntervalEnum interval) {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
 
     }
 }
--- tenok-coin/src/main/java/org/tenok/coin/data/websocket/impl/BybitWebsocketProcessor.java
+++ tenok-coin/src/main/java/org/tenok/coin/data/websocket/impl/BybitWebsocketProcessor.java
@@ -4,26 +4,33 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Date;
 
 import javax.websocket.ContainerProvider;
 import javax.websocket.DeploymentException;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
 
+import org.tenok.coin.data.entity.impl.Candle;
 import org.tenok.coin.data.entity.impl.CandleList;
+import org.tenok.coin.data.entity.impl.OrderList;
 import org.tenok.coin.type.CoinEnum;
 import org.tenok.coin.type.IntervalEnum;
 
 public class BybitWebsocketProcessor implements Closeable {
     private Session websocketSession = null;
+    private BybitWebsocket websocketInstance = null;
+    private Thread heartBeatThread = null;
 
-    public BybitWebsocketProcessor() { }
+    public BybitWebsocketProcessor() {
+    }
 
     public void init() {
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.websocketInstance = new BybitWebsocket();
 
         try {
-            this.websocketSession = container.connectToServer(BybitWebsocket.class, new URI("wss://"));
+            this.websocketSession = container.connectToServer(websocketInstance, new URI("wss://"));
         } catch (DeploymentException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -31,10 +38,49 @@ public class BybitWebsocketProcessor implements Closeable {
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+        heartBeatThread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    websocketSession.getAsyncRemote().sendText("{\"op\":\"ping\"}");
+                    if (interrupted()) {
+                        break;
+                    }
+                    try {
+                        Thread.sleep(30000);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        };
+        heartBeatThread.start();
     }
 
-    public void subscirbeKLine(CoinEnum coinType, IntervalEnum interval, CandleList candleList) {
-        
+    /**
+     * kLine 실시간 처리 등록
+     * @param coinType
+     * @param interval
+     * @param candleList
+     */
+    public void subscribeKLine(CoinEnum coinType, IntervalEnum interval, CandleList candleList) {
+        this.websocketInstance.registerKLineCallback(coinType, interval, (data) -> {
+            double open = (double) data.get("open");
+            double close = (double) data.get("close");
+            double high = (double) data.get("high");
+            double low = (double) data.get("low");
+            double volume = (double) data.get("volume");
+            Date startAt = new Date((long) data.get("start"));
+            boolean confirm = (boolean) data.get("confirm");
+
+            if (confirm) {
+                Candle candle = new Candle(startAt, volume, open, high, low, close);
+                candleList.registerNewCandle(candle);
+            } else {
+                Candle candle = new Candle(startAt, volume, open, high, low, close);
+                candleList.updateCurrentCandle(candle);
+            }
+        });
     }
 
     public void subscribeInsturmentInfo() {
@@ -45,21 +91,18 @@ public class BybitWebsocketProcessor implements Closeable {
 
     }
 
-    public void subscribeOrder() {
+    public void subscribeOrder(CoinEnum coinType, IntervalEnum interval, OrderList orderList) {
 
     }
 
-    public void processKLine() {
-
-    }
-
-    public void processPosition() {
-
+    public void unsubscribeKLine(CoinEnum coinType, IntervalEnum interval) {
+        this.websocketInstance.unregisterKLine(coinType, interval);
     }
 
     @Override
     public void close() throws IOException {
-        // TODO Auto-generated method stub
-
+        this.heartBeatThread.interrupt();   // heart beat 세션 유지용 스레드 인터럽트
+        this.websocketInstance.close();
     }
+
 }
