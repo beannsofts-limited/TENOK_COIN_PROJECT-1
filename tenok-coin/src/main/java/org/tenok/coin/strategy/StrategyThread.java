f5bf9a0f79e02dd501b701b363ad1c8e45e9ec29
lani009 <lani009@naver.com>
2021-01-31 16:13:03 +0900

    upload: backesting strategy viewer

--- tenok-coin/src/main/java/org/tenok/coin/strategy/StrategyThread.java
+++ tenok-coin/src/main/java/org/tenok/coin/strategy/StrategyThread.java
@@ -3,10 +3,12 @@ package org.tenok.coin.strategy;
 import java.lang.reflect.InvocationTargetException;
 
 import org.tenok.coin.data.CoinDataAccessable;
+import org.tenok.coin.data.entity.WalletAccessable;
 
 public class StrategyThread implements Runnable {
     private CoinDataAccessable coinDAOInstance;
     private Strategy strategyInstance;
+    private WalletAccessable wallet;
 
     public StrategyThread(Class<? extends CoinDataAccessable> coinDaoClass, Class<? extends Strategy> strategyClass)
             throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
@@ -16,12 +18,16 @@ public class StrategyThread implements Runnable {
                 .invoke((Object) null, (Object) null);
 
         strategyInstance = strategyClass.getDeclaredConstructor(CoinDataAccessable.class).newInstance(coinDAOInstance);
+        wallet = coinDAOInstance.getWalletInfo();
     }
 
     @Override
     public void run() {
+        
+
         if (strategyInstance.isNotOpened()) {
             double openRBI = strategyInstance.testOpenRBI();
+            double currentAvailable = wallet.getWalletAvailableBalance();
             coinDAOInstance.orderCoin(null);
         } else {
             coinDAOInstance.orderCoin(null);
