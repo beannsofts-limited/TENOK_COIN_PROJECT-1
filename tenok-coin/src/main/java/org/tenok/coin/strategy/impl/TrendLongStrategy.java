cca6c3730b19550313d8dc794e1ac6283198a798
pengm2ng <cjksong@ajou.ac.kr>
2021-02-21 18:58:24 +0900

    TrendStrategy done

--- tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendLongStrategy.java
+++ tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendLongStrategy.java
@@ -1,5 +1,7 @@
 package org.tenok.coin.strategy.impl;
 
+import java.util.Date;
+
 import org.tenok.coin.data.CoinDataAccessable;
 import org.tenok.coin.data.entity.impl.CandleList;
 import org.tenok.coin.data.entity.impl.candle_index.moving_average.MovingAverage;
@@ -14,7 +16,8 @@ public class TrendLongStrategy implements Strategy{
     private CandleList candleList;
     private MovingAverage ma;
     private double entryPrice;
-
+    private Date standardDate = null;
+    
     public TrendLongStrategy(CoinDataAccessable coinDAO, CoinEnum coinType) {
         this.coinDAO = coinDAO;
         this.coinType = coinType;
@@ -26,18 +29,26 @@ public class TrendLongStrategy implements Strategy{
     public double testOpenRBI() {
         //새로운 캔들이 갱신될때마다 봉초가에 open
         //만약 2분선이 20분 선위에 있을 경우에는 무조건 롱 포지션
-        if(ma.getReversed(0).getMa2() > ma.getReversed(0).getMa20()){
-            return 1; 
+        if(standardDate != candleList.getReversed(0).getStartAt()){
+            if(candleList.getReversed(0).getOpen() > ma.getReversed(1).getMa10()){
+                standardDate = candleList.getReversed(0).getStartAt();
+                return 1; 
+            }
         }
         return 0;
+
     }
 
     @Override
     public boolean testCloseRBI() {
         //15분봉의 종가에 청산
-        
-        return true;
+        if(standardDate != candleList.getReversed(0).getStartAt()){   
+            return true;
+        }
+
+        return false;
     }
+
 
     @Override
     public boolean isOpened() {
--- tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendShortStrategy.java
+++ tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendShortStrategy.java
@@ -1,5 +1,7 @@
 package org.tenok.coin.strategy.impl;
 
+import java.util.Date;
+
 import org.tenok.coin.data.CoinDataAccessable;
 import org.tenok.coin.data.entity.impl.CandleList;
 import org.tenok.coin.data.entity.impl.candle_index.moving_average.MovingAverage;
@@ -14,6 +16,7 @@ public class TrendShortStrategy implements Strategy{
     private CandleList candleList;
     private MovingAverage ma;
     private double entryPrice;
+    private Date standardDate = null;
 
     public TrendShortStrategy(CoinDataAccessable coinDAO, CoinEnum coinType) {
         this.coinDAO = coinDAO;
@@ -26,8 +29,11 @@ public class TrendShortStrategy implements Strategy{
     public double testOpenRBI() {
         //새로운 캔들이 갱신될때마다 봉초가에 open
         //만약 2분선이 20분 선 아래에 있을 경우에는 무조건 숏 포지션
-        if(ma.getReversed(0).getMa2() < ma.getReversed(0).getMa20()){
-            return 1; 
+        if(standardDate != candleList.getReversed(0).getStartAt()){
+            if(candleList.getReversed(0).getOpen() <= ma.getReversed(1).getMa10()){
+                standardDate = candleList.getReversed(0).getStartAt();
+                return 1; 
+            }
         }
         return 0;
     }
@@ -35,7 +41,11 @@ public class TrendShortStrategy implements Strategy{
     @Override
     public boolean testCloseRBI() {
          //15분봉의 종가에 청산
-        return true;
+         if(standardDate != candleList.getReversed(0).getStartAt()){   
+            return true;
+        }
+
+        return false;
     }
 
     @Override
