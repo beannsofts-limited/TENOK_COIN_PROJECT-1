799c3d59ecc56a3397d9be4b7bbc060edde3c3f2
lani009 <lani009@naver.com>
2021-02-21 23:51:38 +0900

    remove: secret key

--- tenok-coin/src/main/java/org/tenok/coin/Index.java
+++ tenok-coin/src/main/java/org/tenok/coin/Index.java
@@ -13,25 +13,23 @@ import org.tenok.coin.strategy.StrategyRunner;
 import org.tenok.coin.strategy.impl.LongStrategy;
 import org.tenok.coin.strategy.impl.ShortStrategy;
 import org.tenok.coin.type.CoinEnum;
+import org.tenok.coin.type.SideEnum;
 
 public class Index {
-    public static void main(String[] args) throws IOException, LoginException {
+    public static void main(String[] args) throws LoginException {
         BybitDAO.getInstance().login(args[0]);
 
         StrategyRunner runner = new StrategyRunner();
 
-        StrategyConfig longConfig = new StrategyConfig(CoinEnum.XTZUSDT, BybitDAO.class, LongStrategy.class, 1, 0.3);
-        StrategyConfig shortConfig = new StrategyConfig(CoinEnum.XTZUSDT, BybitDAO.class, ShortStrategy.class, -1, 0.3);
+        StrategyConfig longConfig = new StrategyConfig(CoinEnum.XTZUSDT, SideEnum.OPEN_BUY, BybitDAO.class,
+                LongStrategy.class, 1, 1);
+        StrategyConfig shortConfig = new StrategyConfig(CoinEnum.XTZUSDT, SideEnum.OPEN_SELL, BybitDAO.class,
+                ShortStrategy.class, -1, 1);
         StrategyHandler longHandler = runner.runStrategy(longConfig);
         StrategyHandler shortHandler = runner.runStrategy(shortConfig);
 
+        
         longHandler.start();
         shortHandler.start();
-
-        while (true) {
-            if (false) {
-                break;
-            }
-        }
     }
 }
