241289c260e7b76437bca727944058fd23d5c87e
lani009 <lani009@naver.com>
2021-02-22 23:18:02 +0900

    formatting all

--- tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendLongStrategy.java
+++ tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendLongStrategy.java
@@ -1,6 +1,5 @@
 package org.tenok.coin.strategy.impl;
 
-
 import org.tenok.coin.data.CoinDataAccessable;
 import org.tenok.coin.data.entity.impl.CandleList;
 import org.tenok.coin.data.entity.impl.candle_index.moving_average.MovingAverage;
@@ -8,26 +7,23 @@ import org.tenok.coin.strategy.Strategy;
 import org.tenok.coin.type.CoinEnum;
 import org.tenok.coin.type.IntervalEnum;
 
-
-
-
-public class TrendLongStrategy implements Strategy{
+public class TrendLongStrategy implements Strategy {
     private CoinDataAccessable coinDAO;
     private CoinEnum coinType;
     private boolean isOpened = false;
     private CandleList candleList;
     private MovingAverage ma;
-    private double entryPrice=0.0;
-    
-    private long standardDate = 0 ;
-    
+    private double entryPrice = 0.0;
+
+    private long standardDate = 0;
+
     public TrendLongStrategy(CoinDataAccessable coinDAO, CoinEnum coinType) {
         this.coinDAO = coinDAO;
         this.coinType = coinType;
-        if(coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT){
+        if (coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT) {
             candleList = this.coinDAO.getCandleList(coinType, IntervalEnum.FIFTEEN);
 
-        }else{
+        } else {
 
             candleList = this.coinDAO.getCandleList(coinType, IntervalEnum.FIVE);
         }
@@ -36,16 +32,16 @@ public class TrendLongStrategy implements Strategy{
 
     @Override
     public double testOpenRBI() {
-        //새로운 캔들이 갱신될때마다 봉초가에 open
-        //만약 2분선이 20분 선위에 있을 경우에는 무조건 롱 포지션
-        //timer 실행
+        // 새로운 캔들이 갱신될때마다 봉초가에 open
+        // 만약 2분선이 20분 선위에 있을 경우에는 무조건 롱 포지션
+        // timer 실행
 
-        if(standardDate != candleList.getReversed(0).getStartAt().getTime()){
-            if(candleList.getReversed(0).getOpen() > ma.getReversed(1).getMa10()){
+        if (standardDate != candleList.getReversed(0).getStartAt().getTime()) {
+            if (candleList.getReversed(0).getOpen() > ma.getReversed(1).getMa10()) {
                 standardDate = candleList.getReversed(0).getStartAt().getTime();
                 System.out.println("매매 시간" + standardDate);
                 entryPrice = coinDAO.getCurrentPrice(coinType);
-                return 1; 
+                return 1;
             }
         }
         return 0;
@@ -54,26 +50,23 @@ public class TrendLongStrategy implements Strategy{
 
     @Override
     public boolean testCloseRBI() {
-        //15분봉의 종가에 청산
-        //timer 14분 50초 후 청산
-        if(coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT){
-          
-            if((standardDate + 895000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7 ){   
+        // 15분봉의 종가에 청산
+        // timer 14분 50초 후 청산
+        if (coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT) {
+
+            if ((standardDate + 895000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7) {
                 return true;
             }
 
-        }else{
+        } else {
 
-            if((standardDate + 297000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7 ){   
+            if ((standardDate + 297000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7) {
                 return true;
             }
         }
 
-
-
         return false;
     }
-
 
     @Override
     public boolean isOpened() {
@@ -96,14 +89,13 @@ public class TrendLongStrategy implements Strategy{
     }
 
     private double getProfitPercent() {
-       
+
         return ((coinDAO.getCurrentPrice(coinType) / entryPrice) - 1.0) * 100;
     }
 
     @Override
     public String getStrategyName() {
-        // TODO Auto-generated method stub
         return "추세 롱 전략";
     }
-    
+
 }
--- tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendShortStrategy.java
+++ tenok-coin/src/main/java/org/tenok/coin/strategy/impl/TrendShortStrategy.java
@@ -1,7 +1,5 @@
 package org.tenok.coin.strategy.impl;
 
-
-
 import org.tenok.coin.data.CoinDataAccessable;
 import org.tenok.coin.data.entity.impl.CandleList;
 import org.tenok.coin.data.entity.impl.candle_index.moving_average.MovingAverage;
@@ -10,23 +8,23 @@ import org.tenok.coin.strategy.Strategy;
 import org.tenok.coin.type.CoinEnum;
 import org.tenok.coin.type.IntervalEnum;
 
-public class TrendShortStrategy implements Strategy{
+public class TrendShortStrategy implements Strategy {
     private CoinDataAccessable coinDAO;
     private CoinEnum coinType;
     private boolean isOpened = false;
     private CandleList candleList;
     private MovingAverage ma;
-    private double entryPrice=0.0;
- 
+    private double entryPrice = 0.0;
+
     private long standardDate = 0;
 
     public TrendShortStrategy(CoinDataAccessable coinDAO, CoinEnum coinType) {
         this.coinDAO = coinDAO;
         this.coinType = coinType;
-        if(coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT){
+        if (coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT) {
             candleList = this.coinDAO.getCandleList(coinType, IntervalEnum.FIFTEEN);
 
-        }else{
+        } else {
 
             candleList = this.coinDAO.getCandleList(coinType, IntervalEnum.FIVE);
         }
@@ -35,16 +33,16 @@ public class TrendShortStrategy implements Strategy{
 
     @Override
     public double testOpenRBI() {
-        //새로운 캔들이 갱신될때마다 봉초가에 open
-        //만약 2분선이 20분 선 아래에 있을 경우에는 무조건 숏 포지션
-        if(standardDate != candleList.getReversed(0).getStartAt().getTime()){
-            
-            if(candleList.getReversed(0).getOpen() <= ma.getReversed(1).getMa10()){
-               
+        // 새로운 캔들이 갱신될때마다 봉초가에 open
+        // 만약 2분선이 20분 선 아래에 있을 경우에는 무조건 숏 포지션
+        if (standardDate != candleList.getReversed(0).getStartAt().getTime()) {
+
+            if (candleList.getReversed(0).getOpen() <= ma.getReversed(1).getMa10()) {
+
                 standardDate = candleList.getReversed(0).getStartAt().getTime();
                 System.out.println("매매 시간" + standardDate);
                 entryPrice = coinDAO.getCurrentPrice(coinType);
-                return 1; 
+                return 1;
             }
         }
         return 0;
@@ -52,21 +50,19 @@ public class TrendShortStrategy implements Strategy{
 
     @Override
     public boolean testCloseRBI() {
-         //15분봉의 종가에 청산
-         if(coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT){
-          
-            if((standardDate + 895000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7 ){   
+        // 15분봉의 종가에 청산
+        if (coinType == CoinEnum.BTCUSDT || coinType == CoinEnum.ETHUSDT) {
+
+            if ((standardDate + 895000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7) {
                 return true;
             }
 
-        }else{
+        } else {
 
-            if((standardDate + 297000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7 ){   
+            if ((standardDate + 297000) <= System.currentTimeMillis() || getProfitPercent() <= -0.7) {
                 return true;
             }
         }
-
-
 
         return false;
     }
